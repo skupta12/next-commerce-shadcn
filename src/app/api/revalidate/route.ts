258ae8f7d6de96f526e0806c79ca16c650bdd332
Igor <bichara3333@gmail.com>
2025-02-25 15:56:03 +0200

    webhook

---
+++ src/app/api/revalidate/route.ts
@@ -0,0 +1,6 @@
+import { revalidate } from '@/lib/shopify';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(req: NextRequest): Promise<NextResponse> {
+  return revalidate(req);
+}
