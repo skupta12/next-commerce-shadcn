87f5c5cf3439a0eb4b66a59127e0f99ea289048b
Igor <bichara3333@gmail.com>
2025-02-25 15:30:02 +0200

    revalidate products and collections

--- src/lib/shopify/index.ts
+++ src/lib/shopify/index.ts
@@ -1,3 +1,5 @@
+"use server"
+
 import {
   HIDDEN_PRODUCT_TAG,
   SHOPIFY_GRAPHQL_API_ENDPOINT,
@@ -32,6 +34,9 @@ import {
   ShopifyCartOperation,
 } from "./types";
 import { getCartQuery } from "./queries/cart";
+import { NextRequest, NextResponse } from "next/server";
+import { revalidateTag } from "next/cache";
+import { headers } from "next/headers";
 
 const domain = process.env.SHOPIFY_STORE_DOMAIN
   ? ensureStartsWith(process.env.SHOPIFY_STORE_DOMAIN, "https://")
@@ -397,3 +402,44 @@ export async function addToCart(
 
   return reshapeCart(res.body.data.cartLinesAdd.cart);
 }
+
+// This is called from `app/api/revalidate.ts` so providers can control revalidation logic.
+export async function revalidate(req: NextRequest): Promise<NextResponse> {
+  // We always need to respond with a 200 status code to Shopify,
+  // otherwise it will continue to retry the request.
+  const collectionWebhooks = [
+    'collections/create',
+    'collections/delete',
+    'collections/update'
+  ];
+  const productWebhooks = [
+    'products/create',
+    'products/delete',
+    'products/update'
+  ];
+  const topic = (await headers()).get('x-shopify-topic') || 'unknown';
+  const secret = req.nextUrl.searchParams.get('secret');
+  const isCollectionUpdate = collectionWebhooks.includes(topic);
+  const isProductUpdate = productWebhooks.includes(topic);
+
+  if (!secret || secret !== process.env.SHOPIFY_REVALIDATION_SECRET) {
+    console.error('Invalid revalidation secret.');
+    return NextResponse.json({ status: 401 });
+  }
+
+  if (!isCollectionUpdate && !isProductUpdate) {
+    // We don't need to revalidate anything for any other topics.
+    return NextResponse.json({ status: 200 });
+  }
+
+  if (isCollectionUpdate) {
+    revalidateTag(TAGS.collections);
+  }
+
+  if (isProductUpdate) {
+    revalidateTag(TAGS.products);
+  }
+
+  return NextResponse.json({ status: 200, revalidated: true, now: Date.now() });
+}
+
